16198d9ccf2bd181fc7330511606905acca7f2c4
Daniel Baykov <danibaikov@abv.bg>
2023-03-25 12:36:47 +0200

    adding a Primary Bean

---
+++ Dependency Injection Examples/Spring-DI/src/main/java/baykov/daniel/springdi/services/PrimaryGreetingService.java
@@ -0,0 +1,15 @@
+package baykov.daniel.springdi.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary
+@Service
+public class PrimaryGreetingService implements GreetingService {
+
+
+    @Override
+    public String sayGreeting() {
+        return "Hello World - From the primary bean";
+    }
+}
